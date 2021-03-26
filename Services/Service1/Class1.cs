0cab3f8ab8e82f759bc89630c4579238bd11733d
Vincente <vincente.nguyen@mnltechnology.com>
2021-03-26 19:04:25 +0800

    Init project

---
+++ Services/Service1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Service1
+{
+    public static class Class1
+    {
+        public static async Task<string> Test()
+        {
+            await Task.Delay(500);
+            return JsonConvert.SerializeObject(new {Test = 100});
+        }
+    }
+}
